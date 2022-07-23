9d455d4cea890b485b871285a410d94f05f1b607
Liran Tal <liran.tal@gmail.com>
2022-07-23 22:03:24 +0300

    docs: update example in executable file

--- ruby/ruby-gem-malicious/bin/woof.sh
+++ ruby/ruby-gem-malicious/bin/woof.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 echo "hello";
+
+
+# or can use the following to reference
+# ruby code, and this own gem code if
+# necessary:
+# #!/usr/bin/env ruby
+# require 'woof'
+# puts Woof.hi()
